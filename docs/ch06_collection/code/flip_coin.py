7fca650ee6eafeae74357fb340892fcf6bdf85c9
Ying Liu <yingliu4203@gmail.com>
2024-10-04 17:06:44 -0700

    fix flip_coin.py

--- docs/ch06_collection/code/flip_coin.py
+++ docs/ch06_collection/code/flip_coin.py
@@ -2,13 +2,15 @@ import random
 
 SIDES = ("Head", "Tail")
 
+
 def flip_coin():
 
     # Simulate a coin flip (0 for Heads, 1 for Tails)
     return random.choice(SIDES)
 
+
 def coin_flip_game():
-print("Welcome to the Coin Flip Game!")
+    print("Welcome to the Coin Flip Game!")
 
     # Ask the user to guess the result of the coin flip
     guess = input("Guess the coin flip result (Head/Tail): ").capitalize()
@@ -22,7 +24,6 @@ print("Welcome to the Coin Flip Game!")
     else:
         print(f"Sorry! It's {result}. Better luck next time.")
 
-# Run the game
 
-if **name** == "**main**":
-coin_flip_game()
+if __name__ == "__main":
+    coin_flip_game()
